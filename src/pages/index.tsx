8d67a9697f5975b2c7556ebd631877a562f9c2d4
Samuel Simões <samuelsimoes31@gmail.com>
2022-06-26 16:46:45 -0300

    feat: add input on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import { Button, Flex, FormControl, FormLabel, Stack } from '@chakra-ui/react';
+import Head from 'next/head';
+import Input from '../components/Form/Input';
 
 export default function Home() {
   return (
@@ -6,7 +8,35 @@ export default function Home() {
       <Head>
         <title>Dashgo</title>
       </Head>
-      <div>home</div>
+      <Flex
+        w="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          flexDir="column"
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p={8}
+          borderRadius={8}
+        >
+          <Stack spacing={4}>
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Senha" />
+          </Stack>
+          <Button
+            type="submit"
+            mt={6}
+            colorScheme="pink"
+            size="lg"
+          >
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
     </div>
-  )
+  );
 }
